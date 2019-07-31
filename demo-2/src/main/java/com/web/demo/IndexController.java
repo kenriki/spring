5413cb63ce19970be94fb82b1da1571880ded85b
kenriki <riki.kenmochi@gmail.com>
2019-07-31 17:28:55 +0900

    Spring boot Thymeleaf 静的画面表示＋遷移まで

---
+++ demo-2/src/main/java/com/web/demo/IndexController.java
@@ -0,0 +1,36 @@
+package com.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author rikigigasu
+ */
+@Controller
+@EnableAutoConfiguration
+public class IndexController {
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "hello") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "Hello/hello";
+	}
+
+	@RequestMapping("/")
+	public String root(@RequestParam(value = "name", required = false, defaultValue = "test") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "index";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexController.class, args);
+	}
+
+}
