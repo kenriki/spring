de6c05a7015bb6a3f0ba81e4c8de59b5392b053d
kenriki <riki.kenmochi@gmail.com>
2019-09-08 00:23:31 +0900

    メニューバー追加

--- demo-2/src/main/resources/static/menu.js
+++ demo-2/src/main/resources/static/menu.js
@@ -1,5 +1,48 @@
-var app_menu = app_menu || {
-   init : function (){
-     
+//debugger;
+
+
+var app = app || {};
+
+app.init = function (){
+    var self = this;
+    var html = "";
+    html += "<ul>";
+    
+    //alert(app_menu.data);
+    
+    var arr = self.data;
+    var json_obj = Object.keys(arr).length;
+    
+    debugger;
+    for(var i=1;i<=json_obj;i++){
+      html += "<li><a href="+arr[i]['url']+">"+arr[i]['title']+"</a></li>";
+    }
+    
+    html += "</ul>";
+    
+    document.getElementById("id_menu_bar").innerHTML = html;
+  };
+  
+app.data = {
+	   "1":{
+		   url: "https://www.google.co.jp",
+		   title: "Google"
+	    },
+	    "2":{
+		   url: "https://www.yahoo.co.jp",
+		   title: "Yahoo!"
+	    },
+	    "3":{
+		   url: "https://github.com/kenriki",
+		   title: "Github"
+	    },
+	    "4":{
+		   url: "https://developer.mozilla.org/ja/",
+		   title: "MDN web docs"
+	    },
+	    "5":{
+		   url: "https://developer.ibm.com/technologies/java/",
+		   title: "IBM Developer"
+	    },
+		    
    };
-};
